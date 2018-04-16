549b0848ee3aebc57a69cb116aa138d1111ad02b
chenlj <chenlj@gmail.com>
2018-04-16 20:52:42 +0800

    update combine

--- exe/src/AddTest.cxx
+++ exe/src/AddTest.cxx
@@ -1,4 +1,5 @@
 #include <TFile.h>
+#include <TChain.h>
 #include <TTree.h>
 #include <TH1.h>
 #include <iostream>
@@ -7,42 +8,47 @@
 
 void AddTree(int istart, int iend, int chip_number, std::string in_path, TString out_path){
 
-  //auto outputFile = new TFile(out_path, "RECREATE");
+  auto outputFile = new TFile(out_path, "RECREATE");
+  auto output_tree = std::make_shared<TTree>("clusters", "Seed information for JadePix1" );
 
-  //auto output_tree = std::make_shared<TTree>("clusters", "information for JadePix1");
-  std::vector<int> output_seed_adc;
-  //output_tree->Branch("seed_adc", &output_seed_adc);
+  auto chain = std::make_shared<TChain>("clusters");
 
+  std::vector<int> output_seed_adc; 
+  std::vector<int> output_clus_adc; 
+  output_tree->Branch("seed",&output_seed_adc);
+  output_tree->Branch("cluster",&output_clus_adc);
 
   for(int i=istart; i<iend; i++){
     
     std::cout << "---------> " << i << " <------------" << std::endl;
+    TString infile_name =  Form("%s/WeakFe_CHIPA%d_%d.root",in_path.c_str(),chip_number,i);
 
-    TFile* inputFile = new TFile(Form("%s/WeakFe_CHIPA%d_%d.root",in_path.c_str(),chip_number,i));
-
-    TTree* cluster_tree = static_cast<TTree*>(inputFile->Get("clusters"));
-    if(!cluster_tree) {
-      std::cout << "Could not read tree cluster, cannot continue." << std::endl;
-      return;
-    }
-
-    std::vector<int>* input_seed_adc=0;
-
-    cluster_tree->SetBranchAddress("seed_adc",&input_seed_adc);
-
-    for(Int_t i=0; i<cluster_tree->GetEntries();++i){
-      cluster_tree->GetEntry(i);
-      output_seed_adc.clear();
-      for(auto& seed : (*input_seed_adc)){
-        output_seed_adc.push_back(seed);
-      }
-      //output_tree->Fill();
-    }
-    inputFile->Close();
+    std::cout << "---------> Processing file " << infile_name << std::endl;
+    chain->Add(infile_name);
   }
 
-  //output_tree->Write();
-  //outputFile->Close();
+  std::vector<int>* input_seed_adc=0;
+  std::vector<int>* input_clus_adc=0;
+  chain->SetBranchAddress("seed_adc",&input_seed_adc);
+  chain->SetBranchAddress("cluster_adc",&input_clus_adc);
+
+  for(Int_t i=0; i<chain->GetEntries();i++)
+  {
+    chain->GetEvent(i);
+    output_seed_adc.clear();
+    for(auto& seed : (*input_seed_adc)){
+      output_seed_adc.push_back(seed);
+    }
+
+    output_clus_adc.clear();
+    for(auto& clus : (*input_clus_adc)){
+      output_clus_adc.push_back(clus);
+    }
+
+    output_tree->Fill();
+  }
+  output_tree->Write();
+  outputFile->Close();
 }
 
 void print_usage(){
