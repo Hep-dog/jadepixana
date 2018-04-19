205678e31ed1c18d8626b72b101c3305cb2cd7c7
chenlj <chenlj@gmail.com>
2018-04-19 10:32:50 +0800

    Add Hist

--- exe/src/AddTest.cxx
+++ exe/src/AddTest.cxx
@@ -1,72 +1,60 @@
-#include <TFile.h>
 #include <TChain.h>
-#include <TTree.h>
+#include <TFile.h>
 #include <TH1.h>
+#include <TTree.h>
 #include <iostream>
 #include <string>
 #include <vector>
 
-void AddTree(int istart, int iend, int chip_number, std::string in_path, TString out_path){
-
-  auto outputFile = new TFile(out_path, "RECREATE");
-  auto output_tree = std::make_shared<TTree>("clusters", "Seed information for JadePix1" );
-
+void AddTree(int istart, int iend, int chip_number, std::string in_path, TString out_path)
+{
+  auto output_file = new TFile(out_path, "RECREATE");
   auto chain = std::make_shared<TChain>("clusters");
 
-  std::vector<int> output_seed_adc; 
-  std::vector<int> output_clus_adc; 
-  output_tree->Branch("seed",&output_seed_adc);
-  output_tree->Branch("cluster",&output_clus_adc);
+  for (int i = istart; i < iend; i++) {
 
-  for(int i=istart; i<iend; i++){
-    
     std::cout << "---------> " << i << " <------------" << std::endl;
-    TString infile_name =  Form("%s/WeakFe_CHIPA%d_%d.root",in_path.c_str(),chip_number,i);
+    TString infile_name = Form("%s/WeakFe_CHIPA%d_%d.root", in_path.c_str(), chip_number, i);
 
     std::cout << "---------> Processing file " << infile_name << std::endl;
     chain->Add(infile_name);
   }
 
-  std::vector<int>* input_seed_adc=0;
-  std::vector<int>* input_clus_adc=0;
-  chain->SetBranchAddress("seed_adc",&input_seed_adc);
-  chain->SetBranchAddress("cluster_adc",&input_clus_adc);
+  chain->LoadTree(0);
+  auto out_tree_clone = chain->GetTree()->CloneTree(0);
 
-  for(Int_t i=0; i<chain->GetEntries();i++)
-  {
+  std::cout << "---------> Copying tree content " << std::endl;
+  auto nevents = chain->GetEntries();
+  for (Int_t i = 0; i < nevents; i++) {
+    if (i % static_cast<int>(nevents*0.02) == 0)
+      std::cout << "--------> " 
+                << std::setprecision(3) << std::fixed << i * 100.0 / nevents 
+                << "% <--------" << std::endl;
+
     chain->GetEvent(i);
-    output_seed_adc.clear();
-    for(auto& seed : (*input_seed_adc)){
-      output_seed_adc.push_back(seed);
-    }
-
-    output_clus_adc.clear();
-    for(auto& clus : (*input_clus_adc)){
-      output_clus_adc.push_back(clus);
-    }
-
-    output_tree->Fill();
+    out_tree_clone->Fill();
   }
-  output_tree->Write();
-  outputFile->Close();
+  out_tree_clone->Write();
+  output_file->Close();
 }
 
-void print_usage(){
+void print_usage()
+{
   printf("NAME\n\tAddTest - \n");
   printf("\nJadePixAna\n\tAddTest -c -s -e\n ");
-
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
   if (argc < 2) {
-    print_usage() ;
+    print_usage();
     return -1;
   }
 
   std::string opt_start = "1";
   std::string opt_end = "2";
   std::string opt_chip_number = "1";
-  std::string opt_outputFile = "WeakFe.root";
+  std::string opt_output_file = "WeakFe.root";
   std::string opt_in_path = "output";
 
   for (int i = 1; i < argc; i++) {
@@ -98,7 +86,7 @@ int main(int argc, char** argv) {
     if (opt == "-o") {
       if (i + 1 < argc) {
         i++;
-        opt_outputFile = argv[i];
+        opt_output_file = argv[i];
       }
     }
   }
@@ -106,13 +94,13 @@ int main(int argc, char** argv) {
   int start = static_cast<int>(std::stoul(opt_start));
   int end = static_cast<int>(std::stoul(opt_end));
   int chip_number = static_cast<int>(std::stoul(opt_chip_number));
-  TString outputFile = opt_outputFile;
+  TString output_file = opt_output_file;
   std::string in_path = opt_in_path;
 
-  AddTree(start,end,chip_number,in_path,outputFile);
+  AddTree(start, end, chip_number, in_path, output_file);
 
-  std::cout << "File saved as: \n" << outputFile << std::endl;
+  std::cout << "File saved as: \n"
+            << output_file << std::endl;
 
   return 0;
 }
-
