ebc11525a7879a5d96488a99d5e58cd86be58c08
chenlj <chenlj@gmail.com>
2018-04-22 22:42:22 +0800

    update HistTest

--- exe/src/HistTest.cxx
+++ exe/src/HistTest.cxx
@@ -25,6 +25,7 @@ int main(int argc, char** argv)
   std::string opt_chip_number = "1";
   std::string opt_output_file = "WeakFe.root";
   std::string opt_in_path = "output";
+  std::string opt_source_name = "WeakFe";
 
   for (int i = 1; i < argc; i++) {
     std::string opt(argv[i]);
@@ -46,6 +47,12 @@ int main(int argc, char** argv)
         opt_chip_number = argv[i];
       }
     }
+    if (opt == "-n") {
+      if (i + 1 < argc) {
+        i++;
+        opt_source_name = argv[i];
+      }
+    }
     if (opt == "-i") {
       if (i + 1 < argc) {
         i++;
@@ -65,6 +72,7 @@ int main(int argc, char** argv)
   int chip_number = static_cast<int>(std::stoul(opt_chip_number));
   TString out_path = opt_output_file;
   std::string in_path = opt_in_path;
+  std::string source_name = opt_source_name;
 
   auto output_file = new TFile(out_path, "RECREATE");
   std::vector<TFile*> infile;
@@ -74,7 +82,7 @@ int main(int argc, char** argv)
   for (int i = start; i < end; i++) {
 
     std::cout << "---------> " << i << " <------------" << std::endl;
-    TString infile_name = Form("%s/WeakFe_CHIPA%d_%d.root", in_path.c_str(), chip_number, i);
+    TString infile_name = Form("%s/%s_CHIPA%d_%d.root", in_path.c_str(), source_name.c_str(), chip_number, i);
 
     std::cout << "---------> Processing file " << infile_name << std::endl;
     infile.push_back(new TFile(infile_name));
