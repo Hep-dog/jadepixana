ad0cf9d47b8a46a0c470da7f5247bb016045d314
chenlj <chenlj@gmail.com>
2018-05-17 15:28:14 +0800

    modified:   ../CMakeLists.txt 	modified:   ../exe/CMakeLists.txt 	modified:   ../exe/src/ProjectNPixel.cxx

--- exe/src/ProjectNPixel.cxx
+++ exe/src/ProjectNPixel.cxx
@@ -1,8 +1,8 @@
+#include <TCanvas.h>
 #include <TChain.h>
 #include <TFile.h>
 #include <TH1.h>
 #include <TH2.h>
-#include <TCanvas.h>
 #include <TTree.h>
 #include <iostream>
 #include <string>
@@ -79,45 +79,46 @@ int main(int argc, char** argv)
 
   for (int i = start; i < end; i++) {
 
+    if (i == 5 || i == 6)
+      continue;
     std::cout << "---------> " << i << " <------------" << std::endl;
     TString infile_name = Form("%s/%s_CHIPA%d.root", in_path.c_str(), source_name.c_str(), i);
 
     std::cout << "---------> Processing file " << infile_name << std::endl;
     auto infile = new TFile(infile_name);
 
-    auto hist_clus_size_adc = dynamic_cast<TH2D*>(infile->Get(Form("clus_size_adc_A%d",i)));
-    auto hist_npixels_cluster5x5_adc = dynamic_cast<TH2D*>(infile->Get(Form("npixels_cluster5x5_adc_A%d",i)));
-    auto hist_seed_adc = dynamic_cast<TH1D*>(infile->Get(Form("seed_adc_A%d",i)));
-    auto hist_clus_adc = dynamic_cast<TH1D*>(infile->Get(Form("clus_adc_A%d",i)));
-    auto hist_cluster5x5_adc = dynamic_cast<TH1D*>(infile->Get(Form("cluster5x5_adc_A%d",i)));
-    
+    auto hist_clus_size_adc = dynamic_cast<TH2D*>(infile->Get(Form("clus_size_adc_A%d", i)));
+    auto hist_npixels_cluster5x5_adc = dynamic_cast<TH2D*>(infile->Get(Form("npixels_cluster5x5_adc_A%d", i)));
+    auto hist_seed_adc = dynamic_cast<TH1D*>(infile->Get(Form("seed_adc_A%d", i)));
+    auto hist_clus_adc = dynamic_cast<TH1D*>(infile->Get(Form("clus_adc_A%d", i)));
+    auto hist_cluster5x5_adc = dynamic_cast<TH1D*>(infile->Get(Form("cluster5x5_adc_A%d", i)));
+
     auto sub_dir = output_file->mkdir(Form("A%d", i));
 
     auto sub_pro_dir = sub_dir->mkdir(Form("Projection%d", i));
-    auto c1 = new TCanvas(Form("C_A%d",i),Form("C_A%d",i),800,600);
+    auto c1 = new TCanvas(Form("C_A%d", i), Form("C_A%d", i), 800, 600);
     for (Int_t iBin = 1; iBin <= clus_size; iBin++) {
       auto hpr = dynamic_cast<TH1D*>(hist_clus_size_adc->ProjectionX("", iBin, iBin));
       hpr->SetName(Form("clus_size_adc_A%d_size%d", i, iBin));
 
-      auto hpr_npix = dynamic_cast<TH1D*>(hist_npixels_cluster5x5_adc->ProjectionY("", iBin+1, iBin+1));
+      auto hpr_npix = dynamic_cast<TH1D*>(hist_npixels_cluster5x5_adc->ProjectionY("", iBin + 1, iBin + 1));
       hpr_npix->SetName(Form("npixels_cluster5x5_adc_A%d_%dpixels", i, iBin));
-      
+
       sub_pro_dir->cd();
       hpr->Write();
       hpr_npix->Write();
 
-      if(iBin==1){
+      if (iBin == 1) {
         //hpr->Draw();
         hpr_npix->Draw();
-        hpr_npix->SetLineColor(iBin%8);
-        hpr_npix->GetYaxis()->SetRangeUser(0,8000);
+        hpr_npix->SetLineColor(iBin % 8);
+        hpr_npix->GetYaxis()->SetRangeUser(0, 8000);
         hpr_npix->GetXaxis()->SetTitle("ADC");
-      }else{
+      } else {
         //hpr->Draw("same");
         hpr_npix->Draw("same");
-        hpr_npix->SetLineColor(iBin%8);
+        hpr_npix->SetLineColor(iBin % 8);
       }
-      
     }
 
     sub_dir->cd();
