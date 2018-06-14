f7b5c4af54de1d7cb78f170be893c51ac2457659
frankscu <chenljscu@gmail.com>
2018-06-15 01:05:27 +0800

    add more json conf

---
+++ run/add.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /besfs/groups/higgs/users/chenlj/jadepixana/run
+source ../etc/lxplus_setup.sh
+AddTest -c  1 -s 1 -e 41 -n May_Sr -i output -o May_Sr_Tree_CHIPA1.root
---
+++ run/anajob.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /besfs/groups/higgs/users/chenlj/jadepixana/run
+source ../etc/lxplus_setup.sh
+./ana.py --cs 1 --ce 4
